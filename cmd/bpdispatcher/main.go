ffb932fd88eeac056f98a87eac34caab36e40ab3
Igor Ignatev <brainfair2010@yandex.ru>
2023-01-16 21:30:11 +0200

    push

---
+++ cmd/bpdispatcher/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Webhook struct {
+	InvolvedObject struct {
+		APIVersion string `json:"apiVersion"`
+		Kind       string `json:"kind"`
+		Name       string `json:"name"`
+		Namespace  string `json:"namespace"`
+		UID        string `json:"uid"`
+	} `json:"involvedObject"`
+	Metadata struct {
+		Revision string `json:"kustomize.toolkit.fluxcd.io/revision"`
+	} `json:"metadata"`
+	Severity            string `json:"severity"`
+	Reason              string `json:"reason"`
+	Message             string `json:"message"`
+	ReportingController string `json:"reportingController"`
+	ReportingInstance   string `json:"reportingInstance"`
+	Timestamp           string `json:"timestamp"`
+}
+
+func handleWebhook(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	var data Webhook
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println("API Version:", data.InvolvedObject.APIVersion)
+	fmt.Println("Kind:", data.InvolvedObject.Kind)
+	fmt.Println("Name:", data.InvolvedObject.Name)
+	fmt.Println("Namespace:", data.InvolvedObject.Namespace)
+	fmt.Println("Revision:", data.Metadata.Revision)
+	fmt.Println("Severity:", data.Severity)
+	fmt.Println("Reason:", data.Reason)
+	fmt.Println("Message:", data.Message)
+	fmt.Println("ReportingController:", data.ReportingController)
+	fmt.Println("ReportingInstance:", data.ReportingInstance)
+	fmt.Println("Timestamp:", data.Timestamp)
+	fmt.Fprint(w, "JSON received and parsed!")
+
+	pipeline.triggerPipeline(1, 2, 3, 4, 5)
+}
+
+func main() {
+	http.HandleFunc("/webhook", handleWebhook)
+	http.ListenAndServe(":8000", nil)
+}
